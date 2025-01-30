7518e44dc2b6fb41e5771b66a3dd704d2741b613
Vaibhav <158305194+Vaibhav7960@users.noreply.github.com>
2025-01-30 13:08:57 -0700

    changes

---
+++ cprg306assignments/app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+export default function StudentInfo() {
+    return(
+        <main>
+            <h1>Vaibhav</h1>
+            <section>
+            <Link href='https://github.com/Vaibhav7960/cprg306-assignments.git'>Github-repo</Link>
+            </section>
+        </main>
+    )
+}
