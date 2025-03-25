b782338174c4bf6e106fa8dbc166361541a7ee39
Vaibhav <158305194+Vaibhav7960@users.noreply.github.com>
2025-03-25 15:29:21 -0600

    weel 4

--- cprg306assignments/app/page.js
+++ cprg306assignments/app/page.js
@@ -5,11 +5,16 @@ export default function Page() {
       <h1>CPRG 306:Web Development 2 - Assignments</h1>
       
       <section>
-      <Link href='http://localhost:3000/week-2'>week-2</Link>
+      <Link href='/week-2'>week-2</Link>
       </section>
       <section>
-      <Link href='http://localhost:3000/week-3'>week-3</Link>
+      <Link href='/week-3'>week-3</Link>
       </section>
+      <section>
+      <Link href='/week-4'>week-4</Link>
+      </section>
+
+      
     </main>
   )
 };
